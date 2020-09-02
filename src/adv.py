848e0881abc029109537dbb7282953153b9bae60
Nate Dow <ndow33@gmail.com>
2020-09-02 16:40:01 -0600

    Added rooms

--- src/adv.py
+++ src/adv.py
@@ -9,7 +9,7 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east. An open door looms on the western wall."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -21,6 +21,22 @@ to north. The smell of gold permeates the air."""),
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+
+'bedroom': Room("The Master Bedroom", """Upon entering the open door, you find yourself in a large,
+ornately decorated bedroom. Glass french doors filter in sunlight from the west. 
+An archway with no light sits to the south.
+Another door lies to the north."""),
+
+'bathroom': Room("The Bathroom", """You've found the ever important bathroom. Stop if you need, 
+but otherwise, get back to the bedroom and keep searching! The door now lies to the south of you."""),
+
+'closet': Room("The Closet", """You are now in a closet so large that a family of four
+could live in it comfortably. With fine dresses on one end and suits on the other, this is not
+the treasure you search for. The arch is now to the north of you."""),
+
+'balcony': Room("The balcony", """As you push your way through the french doors and into the 
+crisp evening air, you find that there is nowhere to go but back. Return to the bedroom 
+through the french doors that now lie to the east of you."""),
 }
 
 
@@ -28,19 +44,40 @@ earlier adventurers. The only exit is to the south."""),
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
+
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+room['foyer'].w_to = room['bedroom']
+
 room['overlook'].s_to = room['foyer']
+
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
+
 room['treasure'].s_to = room['narrow']
+
+room['bedroom'].s_to = room['closet']
+room['bedroom'].n_to = room['bathroom']
+room['bedroom'].w_to = room['balcony']
+room['bedroom'].e_to = room['foyer']
+
+room['bathroom'].s_to = room['bedroom']
+
+room['balcony'].e_to = room['bedroom']
+
+room['closet'].n_to = room['bedroom']
+
 
 outside = room['outside']
 foyer = room['foyer']
 overlook = room['overlook']
 narrow = room['narrow']
 treasure = room['treasure']
+bedroom = room['bedroom']
+closet = room['closet']
+bathroom = room['bathroom']
+balcony = room['balcony']
 
 # test = Player('Nate', outside)
 # print(test.current_room.n_to.name)
@@ -75,9 +112,9 @@ while x == 0:
 
     while x == 1:
     # If the user enters a cardinal direction, attempt to move to the room there.
-        print(f'You chose {direction} as your direction')
-        print(player.current_room)
-        print('------------------------------------')
+        # print(f'You chose {direction} as your direction')
+        # print(player.current_room)
+        # print('------------------------------------')
 
         if direction == 'n':
             north_room = player.current_room.n_to
@@ -138,16 +175,8 @@ while x == 0:
             x = 2
         
         else:
+            # Print an error message if the movement isn't allowed.
             print('Unfortunately, that is an invalid direction!')
             direction = input('Your options are n, s, e, w, or q to quit: ')
             print('------------------------------------')
             print('------------------------------------')            
-
-
-
-
-#
-
-# Print an error message if the movement isn't allowed.
-#
-
